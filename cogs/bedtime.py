386df868d98b1ff15dcfac17b15d164a4758df1d
Daneel Olivaw <34151388+R-Olivaw@users.noreply.github.com>
2023-04-19 22:12:25 -0700

    add things

--- cogs/bedtime.py
+++ cogs/bedtime.py
@@ -1,43 +1,43 @@
+import os
 import platform
 import random
 import aiosqlite
 import asyncio
 import aiohttp
 import discord
+from datetime import datetime
 from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
 from helpers import checks
 
-
 class Bedtime(commands.Cog, name="bedtime"):
     def __init__(self, bot):
         self.bot = bot
 
+    async def connect_db(self):
+        db = await aiosqlite.connect(r'C:\Users\Alexa\GitHub\gotobedbot\database\database.db')
+        cursor = await db.cursor()
+        await cursor.execute('CREATE TABLE IF NOT EXISTS reminders (user_id TEXT, time TEXT)')
+        await db.commit()
+        return db
+
     @commands.hybrid_command(
-        name="bedtime", description="Go to bed, bitch."
+        name="bedtime",
+        description="Go to bed, bitch!.",
     )
+    # This will only allow non-blacklisted members to execute the command
     @checks.not_blacklisted()
-    async def set_reminder(self, context: Context):
-        await context.send("What time do you want to go to bed?")
+    async def set_reminder_task(self, time, context: Context):
+        await context.send(f'Reminder set for {time}!')
 
-        def check(msg):
-            return msg.author == context.author and msg.channel == context.channel
+        current_time = datetime.now().strftime('%I:%M%p')
+        delta_time = (datetime.strptime(time, '%I:%M%p') - datetime.strptime(current_time, '%I:%M%p')).total_seconds()
 
-        try:
-            msg = await self.bot.wait_for('message', check=check, timeout=80)
-            time = msg.content
+        await asyncio.sleep(delta_time)
 
-            db = self.bot.init_db()
-            cursor = await db.cursor()
-            await cursor.execute('INSERT INTO reminders (user_id, time) VALUES (?, ?)', (context.author.id, time))
-            await db.commit()
-
-            await context.send(f'Reminder set for {time}!')
-
-        except:
-            await context.send('Something went wrong!')
+        db = await 
 
 async def setup(bot):
     await bot.add_cog(Bedtime(bot))            
